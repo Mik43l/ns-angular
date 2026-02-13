41068102654747f895f05618b694eb4e6bbad19d
Umberto Mercogliano <u.mercogliano@technodesign.it>
2026-02-13 13:17:53 +0100

    Missing s in appUrl

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   apiUrl: 'https://api.nsinstallazioni.it/v1',
-  appUrl: 'http://app.nsinstallazioni.it',
+  appUrl: 'https://app.nsinstallazioni.it',
   nodeUrl: 'https://node.nsinstallazioni.it',
   projectId: '697d1c420027f6b914f8',
   databaseId: '6929ad46001d10f171db',
